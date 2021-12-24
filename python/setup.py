f9c45e7bd058b9cc08d115e29434eaf54b453b23
HPDell <huyg0180110559@outlook.com>
2021-12-24 17:59:54 +0000

    edit: move setup and pyproject to pythom subdir

--- python/setup.py
+++ python/setup.py
@@ -5,9 +5,6 @@ setup(
     version="0.2.0",
     author="Yigong Hu",
     packages=["pygwmodel"],
-    package_dir={
-        "pygwmodel": "python"
-    },
     install_requires=['cython'],
     cmake_args=["-DWITH_PYTHON:BOOL=ON"]
 )
