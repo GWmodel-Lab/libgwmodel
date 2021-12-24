ab790fcab88872dce55f6cefc63a9b8b321c61dc
HPDell <huyg0180110559@outlook.com>
2021-12-24 11:55:47 +0000

    edit: use skbuild

---
+++ setup.py
@@ -0,0 +1,11 @@
+from skbuild import setup
+
+setup(
+    name="pygwmodel",
+    version="0.2.0",
+    author="Yigong Hu",
+    packages=["pygwmodel"],
+    package_dir={
+        "pygwmodel": "python"
+    }
+)
