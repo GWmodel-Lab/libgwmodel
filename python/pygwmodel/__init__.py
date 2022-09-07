45d791488a50585b37f432c34334ce4a7918dfea
HPDell <huyg0180110559@outlook.com>
2022-09-07 06:56:06 +0100

    edit: remove python module

--- python/pygwmodel/__init__.py
+++
@@ -1,5 +0,0 @@
-from .high_api import KernelType, BandwidthSelectionCriterionType
-from .high_api import GWRBasic, GWSS, GWPCA
-
-if __name__ == "__main__":
-    print("PyGWmodel Package.")
--- python/setup.py
+++
@@ -1,9 +0,0 @@
-from skbuild import setup
-
-setup(
-    name="pygwmodel",
-    version="0.2.0",
-    author="Yigong Hu",
-    packages=["pygwmodel"],
-    install_requires=['cython']
-)
