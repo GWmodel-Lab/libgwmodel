7650959c0da3dd282bed3c9148826280024800fb
HPDell <huyg0180110559@outlook.com>
2023-07-14 11:09:53 +0100

    edit: disable threads in TerminateCheckTelegram

--- test/TerminateCheckTelegram.cpp
+++ test/TerminateCheckTelegram.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 queue<string> TerminateCheckTelegram::MessageQueue = queue<string>();
+#ifdef ENABLE_OPENMP
 mutex TerminateCheckTelegram::Lock = mutex();
 bool TerminateCheckTelegram::TerminatePrinter = false;
+#endif
 
 void TerminateCheckTelegram::progress_print()
 {
-    while (!TerminatePrinter)
+#ifdef ENABLE_OPENMP
+   while (!TerminatePrinter)
     {
         unique_lock<mutex> locker(Lock);
         if (MessageQueue.size() > 0)
@@ -19,7 +22,10 @@ void TerminateCheckTelegram::progress_print()
             INFO(msg);
         }
     }
-    
+#else
+    INFO(MessageQueue.front());
+    MessageQueue.pop();
+#endif
 }
 
 void TerminateCheckTelegram::progress(size_t current, size_t total, string fun_name, string file_name)
@@ -27,24 +33,34 @@ void TerminateCheckTelegram::progress(size_t current, size_t total, string fun_n
     (void)current;
     (void)total;
     (void)file_name;
+#ifdef ENABLE_OPENMP
     unique_lock<mutex> locker(Lock);
+#endif
     if (fun_name.find(mBreakStage, 0) >= 0)
     {
         mCancelled = true;
     }
     string ss = string("Progress") + fun_name + ": " + to_string(current) + "/" + to_string(total) + "\n";
     MessageQueue.push(ss);
+#ifndef ENABLE_OPENMP
+    progress_print();
+#endif
 }
 
 void TerminateCheckTelegram::progress(double percent, string fun_name, string file_name)
 {
     (void)percent;
     (void)file_name;
+#ifdef ENABLE_OPENMP
     unique_lock<mutex> locker(Lock);
+#endif
     if (fun_name.find(mBreakStage, 0) >= 0)
     {
         mCancelled = true;
     }
     string ss = string("Progress") + fun_name + ": " + to_string(percent) + "%\n";
     MessageQueue.push(ss);
+#ifndef ENABLE_OPENMP
+    progress_print();
+#endif
 }
