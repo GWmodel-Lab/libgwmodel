797afef01dfa7228f87def1ed26eeef97605129f
HPDell <huyg0180110559@outlook.com>
2023-09-24 15:09:34 +0100

    add: test mat stride mul

--- test/cumat.cpp
+++ test/cumat.cpp
@@ -32,7 +32,7 @@ TEST_CASE("cuBLAS gemm")
         REQUIRE(approx_equal(arC, arD, "absdiff", 1e-6));
     }
 
-    SECTION("stride mat mul")
+    SECTION("stride mul")
     {
         auto flag_A_trans = GENERATE(false, true);
         auto flag_B_trans = GENERATE(false, true);
@@ -48,6 +48,54 @@ TEST_CASE("cuBLAS gemm")
         }
         
         custride cuA(arA), cuB(arB);
+        custride cuC = flag_A_trans ? (flag_B_trans ? (cuA.t() * cuB.t()) : (cuA.t() * cuB)) : (flag_B_trans ? (cuA * cuB.t()) : (cuA * cuB));
+
+        cube arD(arC.n_rows, arC.n_cols, arC.n_slices);
+        cuC.get(arD.memptr());
+        REQUIRE(approx_equal(arC, arD, "absdiff", 1e-6));
+    }
+
+    SECTION("stride mat mul")
+    {
+        auto flag_A_trans = GENERATE(false, true);
+        auto flag_B_trans = GENERATE(false, true);
+        INFO("settings: transport A (" << flag_A_trans << ") transport B (" << flag_B_trans << ")");
+
+        cube arA = flag_A_trans ? cube(k, m, s, arma::fill::randu) : cube(m, k, s, arma::fill::randu);
+        mat arB = flag_B_trans ? mat(n, k, arma::fill::randu) : mat(k, n, arma::fill::randu);;
+        cube arC(m, n, s);
+        for (size_t i = 0; i < s; i++)
+        {
+            mat a = arA.slice(i), b = arB;
+            arC.slice(i) = flag_A_trans ? (flag_B_trans ? mat(trans(a) * trans(b)) : mat(trans(a) * b)) : (flag_B_trans ? mat(a * trans(b)) : mat(a * b));
+        }
+        
+        custride cuA(arA);
+        cumat cuB(arB);
+        custride cuC = flag_A_trans ? (flag_B_trans ? (cuA.t() * cuB.t()) : (cuA.t() * cuB)) : (flag_B_trans ? (cuA * cuB.t()) : (cuA * cuB));
+
+        cube arD(arC.n_rows, arC.n_cols, arC.n_slices);
+        cuC.get(arD.memptr());
+        REQUIRE(approx_equal(arC, arD, "absdiff", 1e-6));
+    }
+
+    SECTION("mat stride mul")
+    {
+        auto flag_A_trans = GENERATE(false, true);
+        auto flag_B_trans = GENERATE(false, true);
+        INFO("settings: transport A (" << flag_A_trans << ") transport B (" << flag_B_trans << ")");
+
+        mat arA = flag_A_trans ? mat(k, m, arma::fill::randu) : mat(m, k, arma::fill::randu);
+        cube arB = flag_B_trans ? cube(n, k, s, arma::fill::randu) : cube(k, n, s, arma::fill::randu);
+        cube arC(m, n, s);
+        for (size_t i = 0; i < s; i++)
+        {
+            mat a = arA, b = arB.slice(i);
+            arC.slice(i) = flag_A_trans ? (flag_B_trans ? mat(trans(a) * trans(b)) : mat(trans(a) * b)) : (flag_B_trans ? mat(a * trans(b)) : mat(a * b));
+        }
+        
+        cumat cuA(arA);
+        custride cuB(arB);
         custride cuC = flag_A_trans ? (flag_B_trans ? (cuA.t() * cuB.t()) : (cuA.t() * cuB)) : (flag_B_trans ? (cuA * cuB.t()) : (cuA * cuB));
 
         cube arD(arC.n_rows, arC.n_cols, arC.n_slices);
