6512fc763a924812ac1fc4328049f7d56da247f8
HPDell <huyg0180110559@outlook.com>
2023-12-13 22:18:13 +0000

    edit: use scatter instead of cast

--- src/gwmodelpp/utils/armampi.cpp
+++ src/gwmodelpp/utils/armampi.cpp
@@ -7,24 +7,18 @@ using namespace arma;
 
 void mat_mul_mpi(mat& a, mat& b, mat& c, const int ip, const int np, const size_t range)
 {
-    arma::uvec a_rows(np, fill::zeros);
-    MPI_Allgather(&b.n_rows, 1, MPI_UNSIGNED_LONG_LONG, a_rows.memptr(), 1, MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD);
-    arma::uvec a_disp = cumsum(a_rows) - a_rows;
-    auto m = a.n_rows, n = b.n_cols, k = a.n_cols;
-    c = mat(m, k, fill::zeros);
-    mat a_buf(range, n, fill::zeros);
+    auto m = a.n_rows, k = a.n_cols;
+    arma::uvec b_rows(np, arma::fill::zeros);
+    MPI_Allgather(&b.n_rows, 1, MPI_UNSIGNED_LONG_LONG, b_rows.memptr(), 1, MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD);
+    arma::Col<int> a_counts = arma::conv_to<arma::Col<int>>::from(b_rows) * a.n_rows;
+    arma::Col<int> a_disp = arma::cumsum(a_counts) - a_counts;
+    c = mat(m, k, arma::fill::zeros);
+    mat a_buf;
     for (size_t pi = 0; pi < np; pi++)
     {
-        if (pi == ip)
-        {
-            a_buf = a;
-        }
-        else
-        {
-            a_buf.resize(a_rows(pi), n);
-        }
-        MPI_Bcast(a_buf.memptr(), a_buf.n_elem, MPI_DOUBLE, pi, MPI_COMM_WORLD);
-        mat ci = a_buf.cols(a_disp(ip), a_disp(ip) + a_rows(ip) - 1) * b;
+        a_buf.resize(b_rows(pi), b_rows(ip));
+        MPI_Scatterv(a.memptr(), a_counts.mem, a_disp.mem, MPI_DOUBLE, a_buf.memptr(), a_buf.n_elem, MPI_DOUBLE, pi, MPI_COMM_WORLD);
+        mat ci = a_buf * b;
         MPI_Reduce(ci.memptr(), c.memptr(), ci.n_elem, MPI_DOUBLE, MPI_SUM, pi, MPI_COMM_WORLD);
     }
 }
