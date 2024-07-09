d1c40d73e2627df2f865b059e8d3c901bdd144a2
HPDell <huyg0180110559@outlook.com>
2024-07-09 17:20:33 +0100

    edit: pre compute b_rows_i

--- src/gwmodelpp/utils/armampi.cpp
+++ src/gwmodelpp/utils/armampi.cpp
@@ -10,11 +10,12 @@ void mat_mul_mpi(mat& a, mat& b, mat& c, const int ip, const int np, const size_
     auto m = a.n_rows, n = b.n_cols;
     arma::uvec b_rows(np, arma::fill::zeros);
     MPI_Allgather(&b.n_rows, 1, MPI_UNSIGNED_LONG_LONG, b_rows.memptr(), 1, MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD);
+    auto b_rows_i = arma::conv_to<arma::Col<int>>::from(b_rows)
     c = mat(m, n, arma::fill::zeros);
     mat a_buf;
     for (int pi = 0; pi < np; pi++)
     {
-        arma::Col<int> a_counts = b_rows(pi) * arma::conv_to<arma::Col<int>>::from(b_rows);
+        arma::Col<int> a_counts = b_rows_i(pi) * b_rows_i;
         arma::Col<int> a_disp = arma::cumsum(a_counts) - a_counts;
         a_buf.resize(b_rows(pi), b_rows(ip));
         MPI_Scatterv(a.memptr(), a_counts.mem, a_disp.mem, MPI_DOUBLE, a_buf.memptr(), a_buf.n_elem, MPI_DOUBLE, pi, MPI_COMM_WORLD);
