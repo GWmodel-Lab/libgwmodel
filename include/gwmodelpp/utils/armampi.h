05e685b3f8d6b3e5137b9f60d73c6a89b934f297
HPDell <huyg0180110559@outlook.com>
2024-07-14 14:58:36 +0100

    edit: rename MY_MPI_UWORD

--- include/gwmodelpp/utils/armampi.h
+++ include/gwmodelpp/utils/armampi.h
@@ -2,9 +2,9 @@
 #include "mpi.h"
 
 #ifdef ARMA_32BIT_WORD
-#define MY_MPI_UWORD MPI_UNSIGNED_LONG
+#define GWM_MPI_UWORD MPI_UNSIGNED_LONG
 #else   // ARMA_32BIT_WORD
-#define MY_MPI_UWORD MPI_UNSIGNED_LONG_LONG
+#define GWM_MPI_UWORD MPI_UNSIGNED_LONG_LONG
 #endif  // ARMA_32BIT_WORD
 
 void mat_mul_mpi(arma::mat& a, arma::mat& b, arma::mat& c, const int ip, const int np, const size_t range);
--- src/gwmodelpp/GWRBasic.cpp
+++ src/gwmodelpp/GWRBasic.cpp
@@ -79,7 +79,7 @@ mat GWRBasic::fit()
             aShape[3] = mWorkRangeSize;
             // cout << mWorkerId << " process send size: [" << aShape[0] << "," << aShape[1] << "," << aShape[2] << "," << aShape[3] << "]\n";
         }
-        MPI_Bcast(aShape, 4, MY_MPI_UWORD, 0, MPI_COMM_WORLD);
+        MPI_Bcast(aShape, 4, GWM_MPI_UWORD, 0, MPI_COMM_WORLD);
         if (mWorkerId > 0)                // slave process
         {
             // cout << mWorkerId << " process recv size: [" << aShape[0] << "," << aShape[1] << "," << aShape[2] << "," << aShape[3] << "]\n";
