ed8db5318f3d0a2e5fb536ca096f18e448d9cb50
HPDell <huyg0180110559@outlook.com>
2024-07-14 11:56:52 +0100

    fix: type mismatch on armadillo and RcppArmadillo

--- include/gwmodelpp/utils/armampi.h
+++ include/gwmodelpp/utils/armampi.h
@@ -1,3 +1,3 @@
-#include <armadillo>
+#include "armadillo_config.h"
 
 void mat_mul_mpi(arma::mat& a, arma::mat& b, arma::mat& c, const int ip, const int np, const size_t range);
--- src/gwmodelpp/utils/armampi.cpp
+++ src/gwmodelpp/utils/armampi.cpp
@@ -7,15 +7,15 @@ using namespace arma;
 
 void mat_mul_mpi(mat& a, mat& b, mat& c, const int ip, const int np, const size_t range)
 {
-    auto m = a.n_rows, n = b.n_cols;
-    arma::uvec b_rows(np, arma::fill::zeros);
-    MPI_Allgather(&b.n_rows, 1, MPI_UNSIGNED_LONG_LONG, b_rows.memptr(), 1, MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD);
-    auto b_rows_i = arma::conv_to<arma::Col<int>>::from(b_rows);
+    arma::uword m = a.n_rows, n = b.n_cols;
+    int k = (int) b.n_rows;
+    arma::Col<int> b_rows(np, arma::fill::zeros);
+    MPI_Allgather(&k, 1, MPI_INT, b_rows.memptr(), 1, MPI_INT, MPI_COMM_WORLD);
     c = mat(m, n, arma::fill::zeros);
     mat a_buf;
     for (int pi = 0; pi < np; pi++)
     {
-        arma::Col<int> a_counts = b_rows_i(pi) * b_rows_i;
+        arma::Col<int> a_counts = b_rows(pi) * b_rows;
         arma::Col<int> a_disp = arma::cumsum(a_counts) - a_counts;
         a_buf.resize(b_rows(pi), b_rows(ip));
         MPI_Scatterv(a.memptr(), a_counts.mem, a_disp.mem, MPI_DOUBLE, a_buf.memptr(), a_buf.n_elem, MPI_DOUBLE, pi, MPI_COMM_WORLD);
